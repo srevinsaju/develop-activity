358cc8a1a0d9f5efb3f3b3c6a306c953b0110176
Srevin Saju <srevin03@gmail.com>
2020-01-01 10:10:41 +0300

    Append integers to the directory if a file aready exists

--- new_activity.py
+++ new_activity.py
@@ -43,9 +43,19 @@ show_launcher = yes
 
 
 def create_activity(name, base_path, skeleton):
-    path = os.path.expanduser(os.path.join(base_path,
-                              '%s.activity' % name.replace(' ', '')))
-    os.makedirs(path)
+    revision = 0
+    while True:
+        try:
+            if revision == 0:
+                revision_string = ""
+            else:
+                revision_string = str(revision)
+            path = os.path.expanduser(os.path.join(base_path,
+                '{}{}.activity'.format(name.replace(' ', ''), revision_string)))
+            os.makedirs(path)
+            break
+        except FileExistsError:
+            revision += 1
     activity_path = os.path.join(path, 'activity')
     os.mkdir(activity_path)
 
